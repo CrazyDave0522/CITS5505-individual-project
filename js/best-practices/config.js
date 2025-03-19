00d06380342dcef3a156283f8fa9a6f6336f948b
CrazyDave0522 <davema0522@gmail.com>
2025-03-19 14:50:32 +0800

    added fallback img

--- js/best-practices/config.js
+++ js/best-practices/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   PRACTICES_URL: "./data/best-practices.json",
   ANIMAL_API: "https://api.thedogapi.com/v1/images/search",
-  FALLBACK_IMAGE: "./images/fallback.png",
+  FALLBACK_IMAGE: "../images/fallback.jpg",
   STORAGE_KEY: "practiceSelections",
   SUCCESS_THRESHOLD: 12,
   CATEGORIES: ["HTML", "CSS", "JS"],
